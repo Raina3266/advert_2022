45124c24c19ce68cf1535c8c861aea51e1f46f8e
Raina <cgl0326@outlook.com>
2026-04-25 16:25:43 +0100

    day 2

--- src/day_2/mod.rs
+++ src/day_2/mod.rs
@@ -1,101 +1,133 @@
+use std::str::FromStr;
 
 #[cfg(test)]
 const TEST_INPUT: &str = include_str!("./test_input.txt");
 pub const INPUT: &str = include_str!("./input.txt");
 
+enum Outcome {
+    Win,
+    Draw,
+    Lose,
+}
+
+impl Outcome {
+    fn score(self) -> i32 {
+        match self {
+            Self::Win => 6,
+            Self::Draw => 3,
+            Self::Lose => 0,
+        }
+    }
+}
+
+enum Shape {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl FromStr for Shape {
+    type Err = String;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        match input {
+            "A" | "X" => Ok(Self::Rock),
+            "B" | "Y" => Ok(Self::Paper),
+            "C" | "Z" => Ok(Self::Scissors),
+            _ => Err("must be A,B,C,X,Y,Z".to_string()),
+        }
+    }
+}
+
+impl Shape {
+    fn score_for_shape(&self) -> i32 {
+        match self {
+            Self::Rock => 1,
+            Self::Paper => 2,
+            Self::Scissors => 3,
+        }
+    }
+    fn outcome(enemy_shape: &Self, my_shape: Shape) -> Outcome {
+        match enemy_shape {
+            Shape::Rock => match my_shape {
+                Shape::Rock => Outcome::Draw,
+                Shape::Paper => Outcome::Win,
+                Shape::Scissors => Outcome::Lose,
+            },
+            Shape::Paper => match my_shape {
+                Shape::Rock => Outcome::Lose,
+                Shape::Paper => Outcome::Draw,
+                Shape::Scissors => Outcome::Win,
+            },
+            Shape::Scissors => match my_shape {
+                Shape::Rock => Outcome::Win,
+                Shape::Paper => Outcome::Lose,
+                Shape::Scissors => Outcome::Draw,
+            },
+        }
+    }
+}
+
+// inherent impl: impl Shape {}
+// trait impl: impl FromStr for Shape {}
+
 pub fn part_1(string: &str) -> i32 {
-    let mut ans = 0;
-    let mut list_of_reports: Vec<Vec<i32>> = vec![];
+    let mut total_score = 0;
     for line in string.lines() {
-        let report = line
-            .split_whitespace()
-            .collect::<Vec<&str>>()
-            .into_iter()
-            .map(|r| r.parse::<i32>().unwrap())
-            .collect();
-        list_of_reports.push(report);
+        let mut iter = line.split(' ');
+        let enemy_shape: Shape = iter.next().unwrap().parse().unwrap();
+        let my_shape: Shape = iter.next().unwrap().parse().unwrap();
+        total_score += my_shape.score_for_shape() + Shape::outcome(&enemy_shape, my_shape).score();
     }
-
-    for report in list_of_reports {
-        // increase
-        if report[0] < report[1] {
-            if check_single_report_is_increase(&report) {
-                ans += 1
-            }
-        // decrease
-        } else if report[0] > report[1]{
-            let new_report: Vec<i32> = report.into_iter().rev().collect();
-            if check_single_report_is_increase(&new_report) {
-                ans += 1;
-            }
-        } else {
-            println!("skip");
-            continue;
-        }
-    }
-    ans
+    total_score
 }
-
-fn check_single_report_is_increase(report: &Vec<i32>) -> bool {
-    for num in 1..report.len() {
-        if report[num - 1] >= report[num] || report[num] - report[num - 1] > 3 {
-            return false
-        }
-    }
-    true
-}
-
 
 #[test]
 fn part_1_test() {
-    assert_eq!(part_1(TEST_INPUT), 2);
+    assert_eq!(part_1(TEST_INPUT), 15);
 }
 
-
-
+fn shape_to_choose(first: &str, result: &str) -> i32 {
+    match result {
+        "X" => match first {
+            "A" => 3,
+            "B" => 1,
+            "C" => 2,
+            _ => unreachable!(),
+        },
+        "Y" => match first {
+            "A" => 1,
+            "B" => 2,
+            "C" => 3,
+            _ => unreachable!(),
+        },
+        "Z" => match first {
+            "A" => 2,
+            "B" => 3,
+            "C" => 1,
+            _ => unreachable!(),
+        },
+        _ => unreachable!(),
+    }
+}
 
 pub fn part_2(string: &str) -> i32 {
-    let mut ans = 0;
-    let mut list_of_reports: Vec<Vec<i32>> = vec![];
+    let mut total_score = 0;
     for line in string.lines() {
-        let report = line
-            .split_whitespace()
-            .collect::<Vec<&str>>()
-            .into_iter()
-            .map(|r| r.parse::<i32>().unwrap())
-            .collect();
-        list_of_reports.push(report);
+        let mut iter = line.split(' ');
+        let round: (&str, &str) = (iter.next().unwrap(), iter.next().unwrap());
+        let point = match round.1 {
+            "X" => shape_to_choose(round.0, "X"),
+            "Y" => shape_to_choose(round.0, "Y") + 3,
+            "Z" => shape_to_choose(round.0, "Z") + 6,
+            _ => unreachable!(),
+        };
+        total_score += point;
     }
-
-    for report in list_of_reports {
-        if check_problem_dampener(&report) {
-                ans += 1;
-        } else {
-            let new_report: Vec<i32> = report.into_iter().rev().collect();
-            if check_problem_dampener(&new_report) {
-                ans += 1;
-            }
-        } 
-    }
-    ans
-}
-
-fn check_problem_dampener(report: &Vec<i32>) -> bool {
-    if !check_single_report_is_increase(report) {
-        for num in 0..report.len() {
-            let mut new_report = report.clone();
-            new_report.remove(num);
-
-            if check_single_report_is_increase(&new_report) {
-                return true;
-            }
-        }
-        return false;
-    } 
-    true
+    total_score
 }
 
 #[test]
 fn part_2_test() {
-    assert_eq!(part_2(TEST_INPUT), 10);
+    assert_eq!(part_2(INPUT), 12);
 }
