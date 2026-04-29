e0b23957efdb46ce67e71e233c0937f3d57f1b78
Raina <cgl0326@outlook.com>
2026-04-29 02:01:11 +0100

    day5

--- src/day_5/mod.rs
+++ src/day_5/mod.rs
@@ -2,46 +2,78 @@
 const TEST_INPUT: &str = include_str!("./test_input.txt");
 pub const INPUT: &str = include_str!("./input.txt");
 
-pub fn part_1(string: &str) -> i32 {
-    let mut count = 0;
-    for pair in string.lines() {
-        let mut group = pair.split(',').map(|elf| {
-            let mut id = elf.split('-').map(|id| id.parse::<i32>().unwrap());
-            (id.next().unwrap(), id.next().unwrap())
-        });
-        let first = group.next().unwrap();
-        let second = group.next().unwrap();
-        if first.0 <= second.0 && first.1 >= second.1 || first.0 >= second.0 && first.1 <= second.1
-        {
-            count += 1;
+struct Ship {
+    crates: Vec<Vec<char>>,
+}
+
+impl Ship {
+    fn number_of_stacks(line: &str) -> usize {
+        (line.chars().count() + 1) / 4
+    }
+
+    fn new(input: &str) -> Ship {
+        let mut crates: Vec<Vec<char>> =
+            vec![vec![]; Ship::number_of_stacks(input.lines().next().unwrap())];
+        for line in input.lines().rev().skip(1) {
+            for n in 0..Ship::number_of_stacks(line) {
+                let chars: Vec<char> = line.chars().collect();
+                if !chars[4 * n + 1].is_ascii_whitespace() {
+                    crates[n].push(chars[4 * n + 1]);
+                }
+            }
+        }
+        Ship { crates }
+    }
+}
+
+#[derive(Debug)]
+struct Step {
+    moves: usize,
+    move_from: usize,
+    move_to: usize,
+}
+
+impl Step {
+    fn new(input: &str) -> Vec<Step> {
+        let mut steps = Vec::new();
+        for line in input.lines() {
+            let iter: Vec<&str> = line.split(' ').collect();
+
+            steps.push(Step {
+                moves: iter[1].parse::<usize>().unwrap(),
+                move_from: iter[3].parse::<usize>().unwrap(),
+                move_to: iter[5].parse::<usize>().unwrap(),
+            });
+        }
+        steps
+    }
+}
+
+pub fn part_1(string: &str) -> Vec<char> {
+    let mut iter = string.split("\n\n");
+    let mut crates = Ship::new(iter.next().unwrap()).crates;
+    let steps = Step::new(iter.next().unwrap());
+
+    for step in steps {
+        for _ in 0..step.moves {
+            let pop = crates[step.move_from - 1].pop().unwrap();
+            crates[step.move_to - 1].push(pop);
         }
     }
-    count
+    let mut ans = vec![];
+    for mut result in crates {
+        ans.push(result.pop().unwrap())
+    }
+    ans
 }
 
 #[test]
 fn part_1_test() {
-    assert_eq!(part_1(INPUT), 2);
+    assert_eq!(part_1(INPUT), vec!['C', 'M', 'Z']);
 }
 
 pub fn part_2(string: &str) -> i32 {
-    let mut count = 0;
-    for pair in string.lines() {
-        let mut group = pair.split(',').map(|elf| {
-            let mut id = elf.split('-').map(|id| id.parse::<i32>().unwrap());
-            (id.next().unwrap(), id.next().unwrap())
-        });
-        let first = group.next().unwrap();
-        let second = group.next().unwrap();
-        if first.0 < second.1 && first.1 >= second.0
-            || second.0 < first.1 && first.0 <= second.1
-            || first.0 == second.0
-            || first.1 == second.1
-        {
-            count += 1;
-        }
-    }
-    count
+    todo!()
 }
 
 #[test]
